02296f58058cfad167839b06d096e5d9da995643
Zhang JiaSai <1556009335@qq.com>
2021-07-14 17:01:43 +0800

    订单路由,创建订单

--- src/routes/order.js
+++ src/routes/order.js
@@ -3,42 +3,41 @@
  * @author Masion
  */
 
- const router = require('koa-router')()
- const { SuccessModel, ErrorModel } = require("../res-model");
+const router = require('koa-router')()
+const { SuccessModel, ErrorModel } = require("../res-model");
 
- //登录校验中间件
+//登录校验中间件
 const loginCheck = require("../middlewares/loginCheck")
 
-  //Shop和Product 的controller方法
-  const {createOrder,getOrderList } = require("../controller/Order")
- 
-  //路由前缀
-  router.prefix('/api/order')
+//Shop和Product 的controller方法
+const { createOrder, getOrderList } = require("../controller/Order")
 
-  //创建订单
-  router.post("/",loginCheck,async(ctx,next)=>{
+//路由前缀
+router.prefix('/api/order')
 
-    const body = ctx.request.body;
-    //创建订单
-    const newOrder = await createOrder(body);
+//创建订单
+router.post("/", loginCheck, async (ctx, next) => {
+    try {
+        const body = ctx.request.body;
+        //创建订单
+        const newOrder = await createOrder(body);
 
-    ctx.body = new SuccessModel(newOrder)
+        ctx.body = new SuccessModel(newOrder)
+    } catch (err) {
+        console.error(err);
+        ctx.body = new ErrorModel(10005, `订单创建失败-${err.message}`);
+    }
 
-  })
+})
 
-  //获取所有订单
-  router.get("/",loginCheck,async(ctx,next)=>{
-      const username = ctx.session.userInfo.username;
-      //查询订单列表
-      const orderList = await getOrderList(username);
+//获取所有订单
+router.get("/", loginCheck, async (ctx, next) => {
+    const username = ctx.session.userInfo.username;
+    //查询订单列表
+    const orderList = await getOrderList(username);
 
-      ctx.body = new SuccessModel(orderList);
-    
-  })
+    ctx.body = new SuccessModel(orderList);
 
+})
 
-
-
-
-
-  module.exports = router
+module.exports = router
